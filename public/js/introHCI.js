b54f37046323d82c8cdc840b804e40427485d6e9
makeitwayne17 <bryantliu17@gmail.com>
2019-03-06 21:55:37 -0800

    auto commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -13,6 +13,9 @@ function initializePage() {
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
+    // document.getElementById(labElementId).backgroundColor = "red";
+
+            // document.getElementById('instructions-txt').style.color = "red";
     var labElement = $(labElementId);
     labElement.css({'background': 'yellow'});
 }
@@ -20,6 +23,7 @@ function onServerResponse(data){
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    console.log("id is:      " +id);
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
